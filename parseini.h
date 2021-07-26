ba9a69c4552233fb0a1a5f0a23361d935262ed69
Tom <elunico@users.noreply.github.com>
2021-07-26 12:51:10 -0400

    Working now?

--- parseini.h
+++ parseini.h
@@ -12,69 +12,92 @@
 #define show_expr(c) \
   std::cout << "[DEBUG :" << __LINE__ << "] " #c << "=" << (c) << std::endl
 
+#define ALL_5(cls, spec)       \
+  cls() = spec;                \
+  cls(cls&) = spec;            \
+  cls(cls&&) = spec;           \
+  cls& operator=(cls&) = spec; \
+  cls& operator=(cls&&) = spec;
+
 namespace tom {
 struct ini_entry;
 struct ini_file;
 
 struct ini_section {
-    ini_file   * owner;
-    ini_section* parent;
-    std::string            name;
-    std::vector<ini_entry*> entries;
+  ini_file* owner;
+  ini_section* parent;
+  std::string name;
+  std::vector<ini_entry*> entries;
 
-    ~ini_section();
+  ~ini_section();
+
+  ALL_5(ini_section, delete);
 };
 
 struct ini_entry {
-    ini_section* parent;
-    std::string key;
-    std::string value;
+  ini_section* parent;
+  std::string key;
+  std::string value;
+
+  ALL_5(ini_entry, delete);
 };
 
 struct ini_file {
-    std::string              name;
-    std::vector<ini_section*> sections;
+  std::string name;
+  std::vector<ini_section*> sections;
 
-    [[nodiscard]] std::vector<ini_section*> const& get_sections() const;
+  [[nodiscard]] std::vector<ini_section*> const& get_sections() const;
 
-    ~ini_file();
+  // FIXME: This is actually what fixed the segfault. Deleting all constructors
+  // and using the default move constructor?
+
+  ini_file() = delete;
+  ini_file(const ini_file&) = delete;
+  ini_file(ini_file&&) = default;
+
+  ini_file& operator=(const ini_file&) = delete;
+  ini_file& operator=(ini_file&&) = delete;
+
+  ~ini_file();
 };
 
 class ini_parser {
-    ini_file* inifile;
-    std::string filename;
-    std::string content;
+  ini_file* inifile;
+  std::string filename;
+  std::string content;
 
-    // imple fields
-    ini_section* current_section_{};
+  // imple fields
+  ini_section* current_section_{};
 
-    void pop_section_();
+  void pop_section_();
 
-    static ini_entry parse_entry(ini_section* parent, std::string& s);
+  static ini_entry parse_entry(ini_section* parent, std::string& s);
 
-    ini_section parse_section(ini_section* parent, std::string& s);
+  ini_section parse_section(ini_section* parent, std::string& s);
 
-    void spleef_whitespace();
+  void spleef_whitespace();
 
-    ini_section* consume_section_optional();
+  ini_section* consume_section_optional();
 
-    ini_entry* consume_entry();
+  ini_entry* consume_entry();
 
-    void consume_to_newline();
+  void consume_to_newline();
 
-public:
-    explicit ini_parser(std::string const& filename);
+  ALL_5(ini_parser, delete);
 
-    ini_file parse();
+ public:
+  explicit ini_parser(std::string const& filename);
 
-    [[nodiscard]] std::string get_filename() const noexcept;
+  ini_file parse();
 
-    ~ini_parser();
+  [[nodiscard]] std::string get_filename() const noexcept;
+
+  ~ini_parser();
 };
 
-std::ostream& operator <<(std::ostream& os, ini_section const& self);
+std::ostream& operator<<(std::ostream& os, ini_section const& self);
 
-std::ostream& operator <<(std::ostream& os, ini_entry const& self);
+std::ostream& operator<<(std::ostream& os, ini_entry const& self);
 }  // namespace tom
 
 #endif  // PARSEINI_PARSEINI_H
